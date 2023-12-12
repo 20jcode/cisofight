4cbfdd4fc8c05cedcc45c5dcf02a23cb968f5c82
mac <20j.code@gmail.com>
2023-12-13 00:00:54 +0900

    save

--- controllers/fight.js
+++ controllers/fight.js
@@ -1,12 +1,16 @@
 const ciso = require('../models/game/ciso');
-const FightRoom = require()
+//const db = require(process.cwd()+'/rdb');
 
 exports.createFight = async (req, res, next) => {
     //새로운 매칭 네임스페이스를 만든다.
 
-
     try {
-        const newFight = await FightRoom.create(); //DB에 새로운 매칭방에 대해 생성
+        //const newFight = await db.execute(); //DB에 새로운 매칭방에 대해 생성
+
+        //db사용 전 테스트용으로 그냥 하드코딩
+        //TODO : db수정
+        const newFight = {_id : 0};
+
         const io = req.app.get('io');
         io.of('/fight').emit('newFight', newFight); //해당 방에 대한 정보를 클라이언트들에게 전달
 
@@ -16,3 +20,4 @@ exports.createFight = async (req, res, next) => {
         next(error);
     }
 };
+
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@
 const express = require('express');
 //const {isLoggedIn, isNotLoggedIn} = require('../middlewares');
 
-const fightController = require('../controllers/fight'); //전투 부분 컨트롤러
+const {createFight} = require('../controllers/fight'); //전투 부분 컨트롤러
 
 const router = express.Router();
 
 
-router.get('/fight',fightController); //전투부분
+router.get('/fight',createFight); //전투부분
 
 module.exports = router;
